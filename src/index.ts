3fb32063f36df52e61ba86307c7c682a83ed6b70
Bogdan <bogdanrazvan00@yahoo.com>
2024-03-06 16:32:03 +0200

    fix: 🐛 exported minmax algo

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {getBestMove, getAllMoves} from "./move-generator";
 
 export {GenericAlgorithm} from "./generic-algo";
 export {randomAlgo} from "./random-algo";
+export {minmaxAlgo} from "./minmax-algo";
---
+++ src/minmax-algo/index.ts
@@ -0,0 +1,1 @@
+export {minmaxAlgo} from "./minmax-algo";
